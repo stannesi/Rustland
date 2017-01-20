ad802e29e0af9165e3508fe698c8f72d94e568b2
Stanley Ilukhor <stannesi@gmail.com>
2017-01-20 12:47:52 -0600

    .: first commit :.

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLevel () {
+		SceneManager.LoadScene("Level01");
+	}
+	public void Credits () {
+		SceneManager.LoadScene("Credits");
+	}
+	public void Back () {
+		SceneManager.LoadScene("MainMenu");
+	}
+	
+
+}
